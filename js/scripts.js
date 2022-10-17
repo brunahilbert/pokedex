9e5c7ccf300b5de1bc92a40b77c3ad142bc59a27
brunahilbert <bruna.hilbertt@gmail.com>
2022-10-17 16:05:15 +0200

    created pokémon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
-alert('Hello world');
-
-let favoriteFood = 'Carrot Cake';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
+    {name: 'Charmander', height: 0.6, type: 'fire'},
+    {name: 'Squirtle', height: 0.5, type: 'water'},
+    {name: 'Caterpie', height: 0.3, type: 'bug'},
+    {name: 'Weedle', height: 0.3, type: ['bug', 'poison']},
+    {name: 'Pidgey', height: 0.3, type: ['flying', 'normal']},
+    {name: 'Rattata', height: 0.3, type: 'normal'},
+    {name: 'Spearow', height: 0.3, type: ['flying', 'normal']},
+    {name: 'Ekans', height: 2, type: 'poison'},
+    {name: 'Pikachu', height: 0.4, type: 'eletric'}
+];
