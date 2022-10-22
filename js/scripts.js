ab1cad7aefd6e88d8068629cdc1a638b47fb1f9d
brunahilbert <bruna.hilbertt@gmail.com>
2022-10-22 13:43:46 +0200

    updated code

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,51 @@
-let pokemonList = [
-    {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
-    {name: 'Charmander', height: 0.6, type: 'fire'},
-    {name: 'Squirtle', height: 0.5, type: 'water'},
-    {name: 'Caterpie', height: 0.3, type: 'bug'},
-    {name: 'Weedle', height: 0.3, type: ['bug', 'poison']},
-    {name: 'Pidgey', height: 0.3, type: ['flying', 'normal']},
-    {name: 'Rattata', height: 0.3, type: 'normal'},
-    {name: 'Spearow', height: 0.3, type: ['flying', 'normal']},
-    {name: 'Ekans', height: 2, type: 'poison'},
-    {name: 'Pikachu', height: 0.4, type: 'eletric'}
-];
 
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > 1.5) {
-        document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that’s big!`);
-    }else {
-        document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height})`);
+// ========== IIFE ========================================//
+
+let pokemonRepository = (function () {
+
+    let pokemonList = [
+        { name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'] },
+        { name: 'Charmander', height: 0.6, type: 'fire' },
+        { name: 'Squirtle', height: 0.5, type: 'water' },
+        { name: 'Caterpie', height: 0.3, type: 'bug' },
+        { name: 'Weedle', height: 0.3, type: ['bug', 'poison'] },
+        { name: 'Pidgey', height: 0.3, type: ['flying', 'normal'] },
+        { name: 'Rattata', height: 0.3, type: 'normal' },
+        { name: 'Spearow', height: 0.3, type: ['flying', 'normal'] },
+        { name: 'Ekans', height: 2, type: 'poison' },
+        { name: 'Pikachu', height: 0.4, type: 'eletric' }
+    ];
+
+
+    function printAll() {
+        pokemonList.forEach(function (list) {
+            if (list.height > 1.5) {
+                document.write(`${list.name} (height: ${list.height}) - Wow, that’s big!`);
+            } else {
+                document.write(`${list.name} (height: ${list.height})`);
+            }
+            document.write('<br>')
+        });
     }
-    document.write('<br>')
-}
+
+    function add(pokemon) {
+        if (typeof pokemon === 'object' &&
+            Object.keys(pokemon).includes('name') &&
+            Object.keys(pokemon).includes('height') &&
+            Object.keys(pokemon).includes('type')) {
+            pokemonList.push(pokemon);
+        } else {
+            console.log('Not valid Pokemon', pokemon)
+        }
+    }
+
+    return {
+        printAll,
+        add
+    }
+})();
+
+pokemonRepository.add({ name: 'Sandshrew', height: 0.6, type: 'ground' });
+pokemonRepository.add({ nawme: 'Sandshrew', height: 0.6, type: 'ground' });
+
+pokemonRepository.printAll();
